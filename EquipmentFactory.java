bc106084c127c881ad8d708c7b61ef1b97498114
Jashira Herrera Brito <herrerabrito.1@buckeyemail.osu.edu>
2024-02-18 19:43:52 -0500

    fixed scanning issue

--- EquipmentFactory.java
+++ EquipmentFactory.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class EquipmentFactory {
-    public static Equipment createEquipment(int serialNumber, String manufacturer, String type) {
+    public static Equipment CreateEquipment(int serialNumber, String manufacturer, String type) {
         return new Equipment(serialNumber, manufacturer, type);
     }
 
@@ -14,12 +14,12 @@ public class EquipmentFactory {
         serialNumber = scanner.nextInt();
 
         System.out.println("manufacturer: ");
-        manufacturer = scanner.nextLine();
+        manufacturer = scanner.next();
 
         System.out.println("type: ");
-        type = scanner.nextLine();
+        type = scanner.next();
         
-        return createEquipment(serialNumber, manufacturer, type);
+        return CreateEquipment(serialNumber, manufacturer, type);
     }
 
 
